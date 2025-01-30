924ab512a8f19c3a069ab443ca9cf6bba94b9c02
michael12345678 <michaelferreiradesousa@gmail.com>
2025-01-30 17:40:05 -0300

    Alteração no Service para incluir na na conexão com a APi o .env

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = process.env.REACT_APP_BACKEND_URL;
+
 
 export const fetchRestaurants = async () => {
   const response = await fetch(`${API_BASE_URL}/restaurants`);
